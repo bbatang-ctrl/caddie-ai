db9a8b61ca0017677b0f260e9c8e4a99b1708097
bbatang-ctrl <bbatang@gmail.com>
2026-04-23 15:15:14 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [
     react({
-      include: ['**/*.jsx', '**/*.tsx', '**/*.js', '**/*.ts'],
+      include: /\.(jsx|js|tsx|ts)$/,
     })
   ],
   build: {
